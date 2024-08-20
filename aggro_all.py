1ea2d8244f6d3fff18a9c23c4db5359f5fcf75de
Null <brunoecrepaldi@gmail.com>
2024-08-19 21:32:39 -0700

    Update aggro_all.py

--- aggro_all.py
+++ aggro_all.py
@@ -2,6 +2,9 @@ __author__ = "NullScript"
 __version__ = "1.0.0" 
 
 from AutoComplete import *
+from AutoComplete import *
+from System.Collections.Generic import List
+from System import Byte
 
 def GetNearbyEnemies(maxRange=1, ignore_list={0x02D4}):
     # Notorieties
@@ -10,7 +13,7 @@ def GetNearbyEnemies(maxRange=1, ignore_list={0x02D4}):
     # 3: 'attackable' ('gray')
     # 4: 'criminal' ('gray')
     # 5: 'enemy' ('orange')
-    # 6: 'murderer' ('red)
+    # 6: 'murderer' ('red')
     # 7: 'npc'
 
     # 0x02D4= Annoying Thing
@@ -24,11 +27,9 @@ def GetNearbyEnemies(maxRange=1, ignore_list={0x02D4}):
 
 
 def AggroAll():       
-    if not Player.WarMode:  
-        return 
-    
     enemies = GetNearbyEnemies(25)
     for enemy in enemies:
+        Mobiles.Message(enemy,0x21,'!!!')
         if enemy != None:
             Player.Attack( enemy )
             Misc.Pause(10)
